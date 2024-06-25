edb0aa41323c1ec102f4b25d6257d10ce803e384
samriddhi <itssamskv@gmail.com>
2024-06-25 20:19:59 +0530

    Update frontend submodule files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom/";
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+import ChatPage from "./Pages/ChatPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/" component={Homepage} exact />'
+      <Route path="/chats" component={ChatPage} />
     </div>
   );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Route path="/" component={Homepage} exact />
+//         <Route path="/chats" component={ChatPage} />
+//       </Router>
+//     </div>
+//   );
+// }
 
 export default App;
